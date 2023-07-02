06a1674226583f31b46e4accba583dc7b15cd317
Mayank Gupta <mayankcpgupta2002@gmail.com>
2023-07-02 22:57:48 +0530

    changes

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 require('dotenv').config()
-// 'mongodb://localhost:27017/mAppDB?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
 const mongoURI = process.env.MONGODB_URI
 
 const connectToMongo = () => {
